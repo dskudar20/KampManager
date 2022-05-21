c0b1a0eb28494184b77580f4a68f1f336f7cde03
David <David@DESKTOP-DAVID>
2022-05-21 15:00:03 +0200

    Add project files.

---
+++ KampManager/Classes/Guest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KampManager.Classes
+{
+    public class Guest : Person
+    {
+        public DateTime BirthDate { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
---
+++ KampManager/Classes/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KampManager.Classes
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PlcmntUnit { get; set; }
+        public DateTime PeriodFrom { get; set; }
+        public DateTime PeriodTo { get; set; }
+        public int NumOfGuests { get; set; }
+        public int GuestPhoneNumber { get; set; }
+
+    }
+}
